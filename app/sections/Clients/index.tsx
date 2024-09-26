d18b7c4385fa356476e0d61e21349bf4fe322ba8
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-09-25 21:42:55 -0300

    Update index.tsx

--- app/sections/Clients/index.tsx
+++ app/sections/Clients/index.tsx
@@ -8,9 +8,9 @@ export default function Clients() {
     return (
         <section className="py-28">
             <div className="max-w-2xl mx-auto flex desktop:flex-row mobile:flex-col justify-center items-center gap-24 relative">
-                <Image className="opacity-60 mobile:w-[50%] mobile:h-[50%]" src={ValverdeLogo} alt="logo da empresa Valverde Engenharia" />
-                <Image className="opacity-40 mobile:w-[60%] mobile:h-[60%]" src={Mv} alt="logo da empresa MV Quiosques" />
-                <Image className="opacity-40 mobile:w-[60%] mobile:h-[60%]" src={VeTech} alt="logo da empresa MV Quiosques"/>
+                <Image className="opacity-60 desktop:w-[200px] mobile:w-[50%] mobile:h-[50%]" src={ValverdeLogo} alt="logo da empresa Valverde Engenharia" />
+                <Image className="opacity-40 desktop:w-[200px] mobile:w-[60%] mobile:h-[60%]" src={Mv} alt="logo da empresa MV Quiosques" />
+                <Image className="opacity-40 desktop:w-[320px] mobile:w-[60%] mobile:h-[60%]" src={VeTech} alt="logo da empresa MV Quiosques"/>
             </div>
         </section>
     )
