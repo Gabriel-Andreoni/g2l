1b959cc627b0d9473bc3f84e03c0c8e9960e53ab
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-11-14 23:06:08 -0300

    Updates na seção de Portfolio

--- app/sections/Portfolio/index.tsx
+++ app/sections/Portfolio/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
 import MvCover from './img/mv-cover.png';
@@ -6,32 +6,49 @@ import ValverdeCover from './img/valverde-cover.png';
 import BlueBellCover from './img/bluebell-cover.png';
 import RMCover from './img/rm-cover.png';
 
+interface ProjectsCover {
+    cover: StaticImageData;
+    url: string;
+}
+
+const projectsCovers: Array<ProjectsCover> = [
+    {
+        cover: MvCover,
+        url: 'https://mvquiosques.com.br/'
+    },
+
+    {
+        cover: ValverdeCover,
+        url: 'https://www.valverdeengenharia.com.br/'
+    },
+
+    {
+        cover: BlueBellCover,
+        url: 'https://bluebell.global/'
+    },
+
+    {
+        cover: RMCover,
+        url: 'https://bluebell.global/'
+    }
+]
+
 export default function Portfolio() {
     return (
         <section className="w-full h-auto flex flex-col relative">
             <h2 className="w-full pb-10 text-center text-3xl font-black uppercase text-[#033B4A]">Nossos últimos projetos</h2>
-            <div className="w-full max-w-7xl desktop:h-full mobile:h-screen mx-auto pb-28 flex desktop:flex-row mobile:flex-col flex-wrap desktop:gap- mobile:gap-4 tablet:gap-4 justify-start after:content-[''] after:w-full desktop:after:h-52 mobile:after:h-[400px] after:block after:absolute after:left-0 desktop:after:top-[15rem] mobile:after:top-[40%] after:bg-[#01d6a3] after:z-[-1]    ">
-                <div className="desktop:w-[30%] mobile:w-80 mobile:mx-auto desktop:mx-0 transition-all duration-500 cursor-pointer hover:shadow-[10px_10px_1px_#033B4A]">
-                    <Link href="https://mvquiosques.com.br/" target="_blank" rel="no-follow">
-                        <Image className="h-full" src={MvCover} alt="site de vendas de quiosques" width="1000" height="100" />
-                    </Link>
-                </div>
-                <div className="desktop:w-[30%] mobile:w-80 mobile:mx-auto transition-all duration-500 cursor-pointer hover:shadow-[10px_10px_1px_#033B4A]">
-                    <Link href="https://www.valverdeengenharia.com.br/" target="_blank" rel="no-follow">
-                        <Image className="h-[209px]" src={ValverdeCover} alt="site de vendas de licença de comércio contra incêndio" width="1000" height="100" />
-                    </Link>
-                </div>
-                <div className="desktop:w-[30%] mobile:w-80 mobile:mx-auto transition-all duration-500 cursor-pointer hover:shadow-[10px_10px_1px_#033B4A]">
-                    <Link href="https://bluebell.global/" target="_blank" rel="no-follow">
-                        <Image className="h-[209px]" src={BlueBellCover} alt="" width="1000" />
-                    </Link>
-                </div>
-
-                <div className="desktop:w-[30%] mobile:w-80 mobile:mx-auto desktop:mx-0 transition-all duration-500 cursor-pointer hover:shadow-[10px_10px_1px_#033B4A]">
-                    <Link href="https://bluebell.global/" target="_blank" rel="no-follow">
-                        <Image className="h-[209px]" src={RMCover} alt="" width="1000" />
-                    </Link>
-                </div>
+            <div className="w-full max-w-7xl desktop:h-full mobile:h-screen mx-auto pb-28 flex desktop:flex-row mobile:flex-col flex-wrap desktop:gap- mobile:gap-4 tablet:gap-4 justify-start after:content-[''] after:w-full desktop:after:h-96 mobile:after:h-[400px] after:block after:absolute after:left-0 desktop:after:top-[15rem] mobile:after:top-[40%] after:bg-[#01d6a3] after:z-[-1]">
+                <ul className="w-full flex flex-wrap gap-4">
+                    {projectsCovers.map((project, index) => {
+                        return (
+                            <li key={index}>
+                                <Link href={project.url} target="_blank" rel="no-follow">
+                                    <Image className="w-[400px] h-[200px]" src={project.cover} alt="Projetos realizados pela G2L" />
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </section>
     )
