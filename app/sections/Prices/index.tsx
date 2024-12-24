582e22a84271204220c890969f6e86c690758d2a
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-12-24 14:59:48 -0300

    Update index.tsx

--- app/sections/Prices/index.tsx
+++ app/sections/Prices/index.tsx
@@ -5,15 +5,76 @@ export function Prices() {
                 <h1 className="text-lg font-black text-[#01D6A3]">Nossos Preços</h1>
                 <h2 className="text-4xl font-black text-[#033b4a]">Investimento que Impulsiona o Seu Sucesso</h2>
             </div>
-            <div className="flex gap-8">
-                <div className="bg-white p-6 rounded-lg shadow-lg">
-                    <div>
-                    <h3 className="text-2xl font-black mb-4">Desenvolvimento de Aplicativos</h3>
-                    <span className="flex flex-col text-xl">
-                        <p className="text-sm text-[#636B6E] font-medium">a partir de</p>
-                        <p className="text-6xl font-black text-[#01D6A3]">R$ 8.000,00</p>
-                    </span>
+            <div className="w-full flex justify-center items-center ">
+                <div className="w-[30%] flex flex-col p-6 rounded-lg shadow-lg">
+                    <div className="mb-4 text-center">
+                        <h3 className="text-2xl font-black">Desenvolvimento de Aplicativos</h3>
+                        <span className="flex flex-col text-xl">
+                            <p className="w-[200px] text-center mt-6 text-sm text-[#636B6E] font-medium">a partir de</p>
+                            <p className="mt-2 text-4xl font-black text-[#01D6A3]">R$ 8.000,00</p>
+                        </span>
                     </div>
+                    
+                    <hr className="my-8" />
+
+                    <div className="w-full text-center">
+                        <h5 className="text-[#636B6E]">Aplicativo para Android ou iOS, com até 3 funcionalidades principais.</h5>
+
+                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
+                            <li className="text-[#636B6E] font-medium text-pretty">Aplicativos nativos ou híbridos para Android e iOS.</li>
+                            <li className="text-[#636B6E] font-medium text-pretty">Integração com APIs e funcionalidades avançadas, como pagamentos, geolocalização e notificações.</li>
+                            <li className="text-[#636B6E] font-medium text-pretty">Suporte inicial após o lançamento.</li>
+                        </ul>
+                    </div>
+
+                    <button className="w-60 mx-auto mt-6 p-4 bg-[#01D6A3] text-white z-50 font-[500] transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]">Saiba mais</button>
+                </div>
+
+                <div className="w-[30%] -mt-4 flex flex-col p-6 rounded-lg shadow-lg bg-[#01D6A3]">
+                    <div className="mb-4 text-center">
+                        <h3 className="text-2xl font-black">Desenvolvimento de Aplicativos</h3>
+                        <span className="flex flex-col text-xl">
+                            <p className="w-[200px] text-center mt-6 text-sm text-[#636B6E] font-medium">a partir de</p>
+                            <p className="mt-2 text-4xl font-black text-white8.000,00</p>
+                        </span>
+                    </div>
+                    
+                    <hr className="my-8" />
+
+                    <div className="text-center">
+                        <h5 className="text-[#636B6E]">Aplicativo para Android ou iOS, com até 3 funcionalidades principais.</h5>
+
+                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
+                            <li className="text-[#636B6E] font-medium">Aplicativos nativos ou híbridos para Android e iOS.</li>
+                            <li className="text-[#636B6E] font-medium">Integração com APIs e funcionalidades avançadas, como pagamentos, geolocalização e notificações.</li>
+                            <li className="text-[#636B6E] font-medium">Suporte inicial após o lançamento.</li>
+                        </ul>
+                    </div>
+
+                    <button className="w-60 mx-auto mt-6 p-4 bg-[#01D6A3] text-white z-50 font-[500] transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]">Saiba mais</button>
+                </div>
+                <div className="w-[30%] flex flex-col p-6 rounded-lg shadow-lg">
+                    <div className="mb-4 text-center">
+                        <h3 className="text-2xl font-black">Desenvolvimento de Aplicativos</h3>
+                        <span className="flex flex-col text-xl">
+                            <p className="w-[200px] text-center mt-6 text-sm text-[#636B6E] font-medium">a partir de</p>
+                            <p className="mt-2 text-4xl font-black text-[#01D6A3]">R$ 8.000,00</p>
+                        </span>
+                    </div>
+                    
+                    <hr className="my-8" />
+
+                    <div className="text-center">
+                        <h5 className="text-[#636B6E]">Aplicativo para Android ou iOS, com até 3 funcionalidades principais.</h5>
+
+                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
+                            <li className="text-[#636B6E] font-medium">Aplicativos nativos ou híbridos para Android e iOS.</li>
+                            <li className="text-[#636B6E] font-medium">Integração com APIs e funcionalidades avançadas, como pagamentos, geolocalização e notificações.</li>
+                            <li className="text-[#636B6E] font-medium">Suporte inicial após o lançamento.</li>
+                        </ul>
+                    </div>
+
+                    <button className="w-60 mx-auto mt-6 p-4 bg-[#01D6A3] text-white z-50 font-[500] transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]">Saiba mais</button>
                 </div>
             </div>
         </section>
