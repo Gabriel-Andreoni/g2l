9bdf4f527fae7b6305621ecf0968735249fcd1db
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-12-26 18:24:09 -0300

    Update index.tsx

--- app/sections/Prices/index.tsx
+++ app/sections/Prices/index.tsx
@@ -1,3 +1,48 @@
+interface PriceProps {
+    name: string,
+    price: string,
+    description: string,
+    features: Array<string>,
+    primary?: boolean,
+}
+
+const services: PriceProps[] = [
+    {
+        name: 'Desenvolvimento de Aplicativos',
+        price: 'R$ 8.000,00',
+        description: 'Aplicativo para Android ou iOS, com até 3 funcionalidades principais.',
+        features: [
+            'Aplicativos nativos ou híbridos para Android e iOS.',
+            'Integração com APIs e funcionalidades avançadas, como pagamentos, geolocalização e notificações.',
+            'Suporte inicial após o lançamento.'
+        ],
+    },
+
+    {
+        name: 'Desenvolvimento de Sites',
+        price: 'R$ 2.000,00',
+        description: 'Site com até 5 páginas, design personalizado e SEO inicial.',
+        features: [
+            'Design personalizado alinhado à identidade visual do cliente.',
+            'Otimização para motores de busca (SEO).',
+            'Integração com ferramentas como Google Analytics, redes sociais e formulários de contato.'
+        ],
+        primary: true,
+    },
+
+    {
+        name: 'Desenvolvimento de Sites em WordPress',
+        price: 'R$ 1.300,00',
+        description: 'Configuração do WordPress, design com tema personalizado e integração avançada.',
+        features: [
+            'Templates otimizados para SEO e compatíveis com dispositivos móveis.',
+            'Ferramentas de fácil gerenciamento para atualizações do site.',
+            'Opção de blogs, lojas virtuais ou portfólios.'
+        ]
+    }
+];
+
+
 export function Prices() {
     return (
         <section className="flex flex-col items-center justify-center min-h-screen p-4">
@@ -6,78 +51,39 @@ export function Prices() {
                 <h2 className="text-4xl font-black text-[#033b4a]">Investimento que Impulsiona o Seu Sucesso</h2>
             </div>
 
-            <div className="w-full my-8 flex mobile:flex-col desktop:gap-1 mobile:gap-16 justify-center items-center ">
-                <div className="desktop:w-[30%] flex flex-col p-6 shadow-2xl">
-                    <div className="mb-4 text-center">
-                        <h3 className="text-2xl font-black text-[#033b4a]">Desenvolvimento de Aplicativos</h3>
-                        <span className="flex flex-col text-xl">
-                            <p className="w-[200px] text-center mt-6 text-sm text-[#636B6E] font-medium">a partir de</p>
-                            <p className="mt-2 text-4xl font-black text-[#01D6A3]">R$ 8.000,00</p>
-                        </span>
-                    </div>
-                    
-                    <hr className="my-8" />
+            <ul className="w-full my-8 flex desktop:flex-row mobile:flex-col desktop:gap-1 mobile:gap-16 justify-center items-center ">
+                {services.map((service, index) => {
+                    return (
+                        <li key={index} className={`desktop:w-[30%] flex flex-col p-6 shadow-2xl ${service.primary ? '-mt-8 bg-[#01D6A3]': ''}`}>
 
-                    <div className="w-full text-center">
-                        <h5 className="text-[#636B6E]">Aplicativo para Android ou iOS, com até 3 funcionalidades principais.</h5>
+                            <div className="mb-4 text-center">
+                                <h3 className="text-2xl font-black text-[#033b4a]">{service.name}</h3>
+                                <span className="flex flex-col text-xl">
+                                    <p className={`w-[200px] text-center mt-6 text-sm ${service.primary ? 'text-white' : 'text-[#636B6E]'} font-medium`}>a partir de</p>
+                                    <p className={`mt-2 text-4xl font-black text-[#01D6A3] ${service.primary ? 'text-white' : 'text-[#01D6A3]'}`}>{service.price}</p>
+                                </span>
+                            </div>
 
-                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
-                            <li className="text-[#636B6E] font-medium text-pretty">Aplicativos nativos ou híbridos para Android e iOS.</li>
-                            <li className="text-[#636B6E] font-medium text-pretty">Integração com APIs e funcionalidades avançadas, como pagamentos, geolocalização e notificações.</li>
-                            <li className="text-[#636B6E] font-medium text-pretty">Suporte inicial após o lançamento.</li>
-                        </ul>
-                    </div>
+                            <hr className="my-8" />
 
-                    <button className="w-60 mx-auto mt-6 p-4 bg-[#01D6A3] text-white font-medium transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]">Saiba Mais</button>
-                </div>
+                            <div className="w-full text-center">
+                                <h5 className={`${service.primary ? 'text-white' : 'text-[#636B6E]'}`}>{service.description}</h5>
 
-                <div className="desktop:w-[30%] -mt-10 flex flex-col p-6 shadow-2xl bg-[#01D6A3]">
-                    <div className="mb-4 text-center">
-                        <h3 className="text-2xl font-black text-[#033b4a]">Desenvolvimento de Sites</h3>
-                        <span className="flex flex-col text-xl">
-                            <p className="w-[200px] text-center mt-6 text-sm text-white font-medium">a partir de</p>
-                            <p className="mt-2 text-4xl font-black text-white">R$ 2.000,00</p>
-                        </span>
-                    </div>
-                    
-                    <hr className="my-8" />
+                                <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
+                                    {service.features.map((feature, index) => {
+                                        return (
+                                            <li key={index} className={`${service.primary ? 'text-white' : 'text-[#636B6E]'} font-medium text-pretty`}>{feature}</li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
 
-                    <div className="text-center">
-                        <h5 className="text-white">Site com até 5 páginas, design personalizado e SEO inicial.</h5>
+                            <button className={`w-60 mx-auto mt-6 p-4 font-medium transition-all ${service.primary ? 'bg-[#033b4a] text-white  hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#fff]' : 'bg-[#033b4a] text-white  hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]'}`}>Saiba Mais</button>
 
-                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
-                            <li className="text-white font-medium">Design personalizado alinhado à identidade visual do cliente.</li>
-                            <li className="text-white font-medium">Otimização para motores de busca (SEO).</li>
-                            <li className="text-white font-medium">Integração com ferramentas como Google Analytics, redes sociais e formulários de contato.</li>
-                        </ul>
-                    </div>
-
-                    <button className="w-60 mx-auto desktop:mt-6 p-4 text-white bg-[#033b4a] font-medium transition-all hover:text-[#01D6A3]">Saiba Mais</button>
-                </div>
-                <div className="desktop:w-[30%] flex flex-col p-6 mobile:mb-16 shadow-2xl">
-                    <div className="mb-4 text-center">
-                        <h3 className="text-2xl font-black text-[#033b4a]">Desenvolvimento de Sites em WordPress</h3>
-                        <span className="flex flex-col text-xl">
-                            <p className="w-[200px] text-center mt-6 text-sm text-[#636B6E] font-medium">a partir de</p>
-                            <p className="mt-2 text-4xl font-black text-[#01D6A3]">R$ 1.300,00</p>
-                        </span>
-                    </div>
-                    
-                    <hr className="my-8" />
-
-                    <div className="text-center">
-                        <h5 className="text-[#636B6E]">Configuração do WordPress, design com tema personalizado e integração avançada.</h5>
-
-                        <ul className="w-full mx-auto mt-4 p-4 flex flex-col gap-4 text-left list-disc">
-                            <li className="text-[#636B6E] font-medium">Templates otimizados para SEO e compatíveis com dispositivos móveis.</li>
-                            <li className="text-[#636B6E] font-medium">Ferramentas de fácil gerenciamento para atualizações do site.</li>
-                            <li className="text-[#636B6E] font-medium">Opção de blogs, lojas virtuais ou portfólios.</li>
-                        </ul>
-                    </div>
-
-                    <button className="w-60 mx-auto mt-6 p-4 bg-[#01D6A3] text-white font-medium transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#01D6A3]">Saiba Mais</button>
-                </div>
-            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </section>
     );
 };
