bf83df1f25746dc0398db7a9acdcbf5a57108f0d
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-12-29 18:28:22 -0300

    Updates para o modo responsívo

--- app/sections/Clients/index.tsx
+++ app/sections/Clients/index.tsx
@@ -11,7 +11,7 @@ export default function Clients() {
             <div className="max-w-2xl mx-auto flex desktop:flex-row mobile:flex-col justify-center items-center gap-24 relative">
                 <Image className="opacity-60 desktop:w-[180px] mobile:w-[50%] mobile:h-[50%]" src={ValverdeLogo} alt="logo da empresa Valverde Engenharia" />
                 <Image className="opacity-40 desktop:w-[190px] mobile:w-[60%] mobile:h-[60%]" src={Mv} alt="logo da empresa MV Quiosques" />
-                <Image className="opacity-40 desktop:w-[90px] mobile:w-[60%] mobile:h-[60%]" src={Bluebell} alt="logo da empresa Bluebell Global"/>
+                <Image className="opacity-40 desktop:w-[90px] mobile:w-[40%] mobile:h-[40%]" src={Bluebell} alt="logo da empresa Bluebell Global"/>
                 <Image className="opacity-40 desktop:w-[150px] mobile:w-[60%] mobile:h-[60%]" src={RM} alt="logo da empresa RM Ressonância Magnética"/>
             </div>
         </section>
--- app/sections/Portfolio/index.tsx
+++ app/sections/Portfolio/index.tsx
@@ -38,7 +38,7 @@ export default function Portfolio() {
         <section className="w-full h-auto flex flex-col relative">
             <h2 className="w-full pb-10 text-center text-3xl font-black uppercase text-[#033B4A]">Nossos últimos projetos</h2>
             <div className="w-full max-w-7xl desktop:h-full mobile:h-screen mx-auto pb-28 flex desktop:flex-row mobile:flex-col flex-wrap desktop:gap- mobile:gap-4 tablet:gap-4 justify-start after:content-[''] after:w-full desktop:after:h-96 mobile:after:h-[400px] after:block after:absolute after:left-0 desktop:after:top-[15rem] mobile:after:top-[40%] after:bg-[#01d6a3] after:z-[-1]">
-                <ul className="w-full flex flex-wrap gap-4">
+                <ul className="w-full flex flex-wrap gap-4 desktop:p-0 mobile:p-4">
                     {projectsCovers.map((project, index) => {
                         return (
                             <li className="transition-all hover:bg-[#033b4a] hover:text-[#01D6A3] hover:shadow-[10px_10px_1px_#033b4a]"key={index}>
