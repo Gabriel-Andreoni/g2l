d19d9413d19c75b8bb18c1b8ee067166bbcc06ec
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-12-29 16:15:51 -0300

    Update index.tsx

--- app/sections/Prices/index.tsx
+++ app/sections/Prices/index.tsx
@@ -53,7 +53,7 @@ export function Prices() {
                 <h2 className="text-4xl font-black text-[#033b4a]">Investimento que Impulsiona o Seu Sucesso</h2>
             </div>
 
-            <ul className="w-full h-screen my-8 flex flex-wrap gap-8 desktop:flex-row mobile:flex-col justify-start items-center">
+            <ul className="w-full h-screen my-8 flex desktop:flex-wrap mobile:flex-nowrap gap-8 desktop:flex-row mobile:flex-col justify-start items-center">
 
                 {services.map((service, index) => {
                     return (
