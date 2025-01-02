5dc38253b4f38fdd3641a8cbf52b694b63b24128
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2025-01-02 15:44:51 -0300

    Adição de ícones e iframe no footer

--- app/sections/Footer/index.tsx
+++ app/sections/Footer/index.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
-import Logo from "../Menu/img/gl2-logo.png"
 import Link from "next/link";
+
+import Logo from "../Menu/img/gl2-logo.png";
+import PhoneIcon from './img/phone-call.png';
+import EmailIcon from './img/email.png';
+
 
 export default function Footer() {
     return (
@@ -42,13 +46,25 @@ export default function Footer() {
                 <h2 className="text-[#01D6A3] font-black text-xl">Contato</h2>
                 <ul className="w-full flex flex-col gap-2">
                     <li className="flex items-center gap-2">
-                        <span className="w-4 h-[2px] bg-[#01D6A3] block"></span>
+                        <Image src={PhoneIcon} alt="ícone de um telefone" width="20" height="20" />
                         <span className="text-[#fff] font-medium">+55 19 997010883</span>
                     </li>
                     <li className="flex items-center gap-2">
-                        <span className="w-4 h-[2px] bg-[#01D6A3] block"></span>
-                        <span className="text-[#fff] font-medium">g2l.clients@gmail.com</span>
+                        <Image src={EmailIcon} alt="ícone de email" width="20" height="20" />
+                        <span className="text-[#fff] font-medium">
+                            <Link href="mailto:g2l.clients@gmail.com?subject=Orçamento&body=Olá,%20gostaria%20de%20solicitar%20um%20orçamento.">
+                                g2l.clients@gmail.com
+                            </Link>
+                        </span>
                     </li>
+
+                    <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14720.251256647203!2d-47.63321423972503!3d-22.72590654873897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c631b0ffcd3e87%3A0x7e234a0f13c70ac0!2sTv.%20Gilberto%20Amado%2C%2042%20-%20Vila%20Monteiro%2C%20Piracicaba%20-%20SP%2C%2013418-415!5e0!3m2!1spt-PT!2sbr!4v1735843085605!5m2!1spt-PT!2sbr"
+                            width="100%"
+                            height="100%"
+                            className="mt-4 border-none"
+                            allowFullScreen
+                        />
                 </ul>
             </div>
         </section>
